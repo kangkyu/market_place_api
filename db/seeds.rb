6a563d3acf88b339328714b28eae0f280ad7b504
kangkyu <kangkyu1111@gmail.com>
2015-07-10 16:48:46 -0700

    seed file for a user data and curl command

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create({email: "example@marketplace.com",
+                          password: "12345678",
+                          password_confirmation: "12345678"})
