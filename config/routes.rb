0bf33a426784ec13f0f2f9d41803bf62e1719340
kangkyu <kangkyu1111@gmail.com>
2015-07-10 16:09:16 -0700

    versioning with api_constraints class

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+require 'api_constraints'
+
 MarketPlaceApi::Application.routes.draw do
+
+# we are going to set the constraints,
+# the base_url and the default response format
+# for each request.
+
+namespace :api, defaults: { format: :json },
+                constraints: { subdomain: 'api' },
+                path: '/' do
+  scope module: :vi,
+        constraints: ApiConstraints.new(version: 1, default: true) do
+    # we are going to list our resources.
+  end
+end
 
 end
