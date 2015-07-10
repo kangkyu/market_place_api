1c0b8881bbe94b38d1d50c0f4f29dc2ccb52a2c1
kangkyu <kangkyu1111@gmail.com>
2015-07-10 16:43:47 -0700

    build the show action for the user

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ MarketPlaceApi::Application.routes.draw do
 namespace :api, defaults: { format: :json },
                 constraints: { subdomain: 'api' },
                 path: '/' do
-  scope module: :vi,
+  scope module: :v1,
         constraints: ApiConstraints.new(version: 1, default: true) do
     # we are going to list our resources.
+    resources :users, only: [:show]
   end
 end
 
